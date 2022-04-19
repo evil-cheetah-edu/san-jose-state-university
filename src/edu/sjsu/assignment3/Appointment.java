95b8803f0969c3811e96936305383ea2ca992d94
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-04-18 17:33:25 -0700

    update: implement abstract appointment

--- src/edu/sjsu/assignment3/Appointment.java
+++ src/edu/sjsu/assignment3/Appointment.java
@@ -1,4 +1,24 @@
 package edu.sjsu.assignment3;
 
-public class Appointment {
+import java.time.LocalDate;
+
+
+public abstract class Appointment
+{
+    private final String    description;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Appointment(String description, LocalDate start_date, LocalDate end_date)
+    {
+        this.description = description;
+        this.startDate   = start_date;
+        this.endDate     = end_date;
+    }
+
+    public String    getDescription() { return this.description; }
+    public LocalDate getStartDate()   { return this.startDate;   }
+    public LocalDate getEndDate()     { return this.endDate;     }
+
+    public abstract boolean occursOn(LocalDate date);
 }
