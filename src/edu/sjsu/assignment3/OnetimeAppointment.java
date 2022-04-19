10b600e7e24509a5337deb89f0940b18edc892cd
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-04-18 17:33:46 -0700

    update: implement OneTimeAppointment appointment

--- src/edu/sjsu/assignment3/OnetimeAppointment.java
+++ src/edu/sjsu/assignment3/OnetimeAppointment.java
@@ -1,7 +1,18 @@
 package edu.sjsu.assignment3;
 
+import java.time.LocalDate;
 
-public class OnetimeAppointment
+
+public class OnetimeAppointment extends Appointment
 {
+    public OnetimeAppointment(String description, LocalDate start_date)
+    {
+        super(description, start_date, start_date);
+    }
 
+    @Override
+    public boolean occursOn(LocalDate date)
+    {
+        return date.isEqual( getStartDate() );
+    }
 }
