f509f33cf173c2dd295e7e155c33b84975867e94
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-04-18 18:54:26 -0700

    implement: fully

--- src/edu/sjsu/assignment3/MonthlyAppointment.java
+++ src/edu/sjsu/assignment3/MonthlyAppointment.java
@@ -1,7 +1,22 @@
 package edu.sjsu.assignment3;
 
+import java.time.LocalDate;
 
-public class MonthlyAppointment
+
+public class MonthlyAppointment extends Appointment
 {
+    public MonthlyAppointment(String description, LocalDate start_date, LocalDate end_date)
+    {
+        super(description, start_date, end_date);
+    }
 
+    @Override
+    public boolean occursOn(LocalDate date)
+    {
+        return (
+            !date.isBefore( getStartDate() ) &&
+            !date.isAfter(  getEndDate()   ) &&
+            (date.getDayOfMonth() == getStartDate().getDayOfMonth())
+        );
+    }
 }
