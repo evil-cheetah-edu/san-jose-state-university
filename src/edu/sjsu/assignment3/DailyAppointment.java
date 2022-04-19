e00824b3332eb1d93e3d0adbc3ae48c3b2855032
Eugene Moshchyn <eugene.moschyn@gmail.com>
2022-04-18 18:54:18 -0700

    implement: fully

--- src/edu/sjsu/assignment3/DailyAppointment.java
+++ src/edu/sjsu/assignment3/DailyAppointment.java
@@ -1,7 +1,21 @@
 package edu.sjsu.assignment3;
 
+import java.time.LocalDate;
 
-public class DailyAppointment
+
+public class DailyAppointment extends Appointment
 {
+    public DailyAppointment(String description, LocalDate start_date, LocalDate end_date)
+    {
+        super(description, start_date, end_date);
+    }
 
+    @Override
+    public boolean occursOn(LocalDate date)
+    {
+        return (
+            !date.isBefore( getStartDate() ) &&
+            !date.isAfter(  getEndDate()   )
+        );
+    }
 }
